f682f6d93b606d68f9d964947209b0c290c6d971
Mike Saunby <mike@saunby.net>
2016-11-13 19:27:37 +0000

    web socket

--- www/index.js
+++ www/index.js
@@ -4,6 +4,19 @@ app.use(express.static('static'));
 
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var Transform = require('stream').Transform,
+    csv = require('csv-streamify');
+
+var csvIn = csv({objectMode: true});
+
+var parser = new Transform({objectMode: true});
+
+parser._transform = function(data, encoding, done) {
+  //this.push("row " + data[0] + ":" + data[1] + "\n");
+  io.emit('xyz', { x:data[0], y:data[1], z:data[2] });
+  done();
+};
+
 
 io.on('connection', function(socket){
 
@@ -11,19 +24,24 @@ io.on('connection', function(socket){
 });
 
 const spawn = require('child_process').spawn;
-const accel = spawn('ls', ['-l', '/bin']);
+const accel = spawn('../accel_stream', []);
 
-accel.stdout.on('data', (data) => {
-    console.log(`stdout: ${data}`);
-});
+//accel.stdout.on('data', (data) => {
+//    console.log(`stdout: ${data}`);
+//});
     
+accel.stdout
+.pipe(csvIn)
+.pipe(parser);
+//.pipe(process.stdout);
+
 
 
 var counter = 0;
 // start interval timer.
 var interval = setInterval( function() {
     //console.log('timer', counter);
-    io.emit('timer', { for: 'everyone', count: counter });
+    //io.emit('timer', { for: 'everyone', count: counter });
     counter++;
     if (counter >= 20000) {
 	clearInterval(interval);
